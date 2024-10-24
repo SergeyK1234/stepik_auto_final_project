3ced098f7ae16c95601a8787e1b86943fb21abae
Сергей Коновцев <skonovtsev@yandex.ru>
2024-10-25 00:26:12 +0200

    add basepage locators, add test to product page

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ import pytest
 from selenium.webdriver.common.by import By
 
 from pages.product_page import ProductPage
+from pages.login_page import LoginPage
 from pages.locators import ProductPageLocators
 
 
@@ -28,26 +29,42 @@ PRODUCT_LINK = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_2
 #     page.title_should_be_in_success_message()
 #     page.price_should_be_equal
 
-@pytest.mark.skip
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, PRODUCT_LINK)
+# @pytest.mark.skip
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, PRODUCT_LINK)
+#     page.open()
+#     page.click_on_add_to_cart_button()
+#     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+#         'Element is present'
+
+
+# def test_guest_cant_see_success_message(browser):
+#     page = ProductPage(browser, PRODUCT_LINK)
+#     page.open()
+#     assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
+#         'Element is present'
+
+
+# @pytest.mark.skip
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     page = ProductPage(browser, PRODUCT_LINK)
+#     page.open()
+#     page.click_on_add_to_cart_button()
+#     assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
+#         'Element is present'
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
     page.open()
-    page.click_on_add_to_cart_button()
-    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
-        'Element is present'
+    page.should_be_login_link()
 
 
-def test_guest_cant_see_success_message(browser):
-    page = ProductPage(browser, PRODUCT_LINK)
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
     page.open()
-    assert page.is_not_element_present(*ProductPageLocators.SUCCESS_MESSAGE), \
-        'Element is present'
-
-
-@pytest.mark.skip
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    page = ProductPage(browser, PRODUCT_LINK)
-    page.open()
-    page.click_on_add_to_cart_button()
-    assert page.is_disappeared(*ProductPageLocators.SUCCESS_MESSAGE), \
-        'Element is present'
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
